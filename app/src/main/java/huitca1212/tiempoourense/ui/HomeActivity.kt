5c615265125ce3338c660d4a5f57ebef06a87603
Rodrigo Tugores <rodrigo.tugores@accenture.com>
2018-07-11 11:48:15 +0200

    Fixed code in the constraint layout of the station details.

--- app/src/main/java/huitca1212/tiempoourense/ui/HomeActivity.kt
+++ app/src/main/java/huitca1212/tiempoourense/ui/HomeActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 import huitca1212.tiempoourense.R
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_home.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
         setSupportActionBar(toolbar)
 
