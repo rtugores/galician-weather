8c68ca129614b549f55ea071c53ed1e802f5e4a6
Rodrigo Tugores <rodrigo.tugores@accenture.com>
2019-02-11 19:07:35 +0100

    Updated libraries, fixed critical issue in android pie, and fixed negative temperatures.

--- app/src/main/java/huitca1212/galicianweather/data/repository/LastMinutesInfoRepository.kt
+++ app/src/main/java/huitca1212/galicianweather/data/repository/LastMinutesInfoRepository.kt
@@ -1,12 +1,8 @@
 package huitca1212.galicianweather.data.repository
 
 import huitca1212.galicianweather.data.datasource.LastMinutesInfoNetworkDataSource
-import huitca1212.galicianweather.data.datasource.model.DataLastMinutesWrapper
-import huitca1212.galicianweather.domain.Callback
 
-class LastMinutesInfoRepository(
-    private val dataSource: LastMinutesInfoNetworkDataSource
-) {
+class LastMinutesInfoRepository(private val dataSource: LastMinutesInfoNetworkDataSource) {
 
     fun getLastMinutesInfo(idEst: String) =
         dataSource.getLastMinutesInfo(idEst)
--- app/src/main/java/huitca1212/galicianweather/domain/DataPolicy.kt
+++
@@ -1,6 +0,0 @@
-package huitca1212.galicianweather.domain
-
-interface DataPolicy
-
-object Local : DataPolicy
-object Network: DataPolicy
